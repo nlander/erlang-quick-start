62bd29d1d1223722debf69bc2dee2cf3aca6f313
Nathan Lander <lander89@gmail.com>
2017-06-17 17:51:35 -0500

    Lists and Tuples

--- tut.erl
+++ tut.erl
@@ -1,5 +1,5 @@
 -module(tut).
--export([double/1, fac/1, mult/2]).
+-export([double/1, fac/1, mult/2, convert/2, convert_length/1, list_length/1]).
 
 double(X) ->
   2 * X.
@@ -11,3 +11,19 @@ fac(N) ->
 
 mult(X, Y) ->
   X * Y.
+
+convert(M, inch) ->
+  M / 2.54;
+
+convert(N, centimeter) ->
+  N * 2.54.
+
+convert_length({centimeter, X}) ->
+  {inch, X / 2.54};
+convert_length({inch, Y}) ->
+  {centimeter, Y * 2.54}.
+
+list_length([]) ->
+  0;
+list_length([First | Rest]) ->
+  1 + list_length(Rest).
