ea917835c63faceb2a047b077295bf55d5083bf6
Nathan Lander <lander89@gmail.com>
2017-06-19 00:07:26 -0500

    Larger example section 2.9

--- tut.erl
+++ tut.erl
@@ -1,5 +1,5 @@
 -module(tut).
--export([double/1, fac/1, mult/2, convert/2, convert_length/1, list_length/1]).
+-export([double/1, fac/1, mult/2, convert/2, convert_length/1, list_length/1, format_temps/1]).
 
 double(X) ->
   2 * X.
@@ -25,5 +25,19 @@ convert_length({inch, Y}) ->
 
 list_length([]) ->
   0;
-list_length([First | Rest]) ->
+list_length([_ | Rest]) ->
   1 + list_length(Rest).
+
+format_temps([]) ->
+  ok;
+format_temps([City | Rest]) ->
+  print_temp(convert_to_celcius(City)),
+  format_temps(Rest).
+
+convert_to_celcius({Name, {c, Temp}}) -> % No conversion necessary
+  {Name, {c, Temp}};
+convert_to_celcius({Name, {f, Temp}}) -> % Do the conversion
+  {Name, {c, (Temp - 32) * 5 / 9}}.
+
+print_temp({Name, {c, Temp}}) ->
+  io:format("~-15w ~w c~n", [Name, Temp]).
