e9ecfbdab91069fdadc2bf4fdcc050d72fcf2c98
Nathan Lander <lander89@gmail.com>
2017-06-18 23:07:58 -0500

    Fix typo

--- color.erl
+++ color.erl
@@ -2,9 +2,12 @@
 
 -export([new/4, blend/2]).
 
--define(is_channel(V), (is_float(V), andalso V >= 0.0 andalso V =< 1.0)).
+-define(is_channel(V), (is_float(V) andalso V >= 0.0 andalso V =< 1.0)).
 
-new(R,G,B,A) ->
+new(R,G,B,A) when ?is_channel(R)
+                , ?is_channel(G)
+                , ?is_channel(B)
+                , ?is_channel(A) ->
     #{red => R, green => G, blue => B, alpha => A}.
 
 blend(Src,Dst) ->
@@ -13,8 +16,14 @@ blend(Src,Dst) ->
 blend(Src,Dst,Alpha) when Alpha > 0.0 ->
   Dst#{ red   := red(Src,Dst) / Alpha
       , green := green(Src,Dst) / Alpha
-      , blue := blue(Src,Dst) / Alpha
+      , blue  := blue(Src,Dst) / Alpha
       , alpha := Alpha
+      };
+blend(_,Dst,_) ->
+  Dst#{ red   := 0.0
+      , green := 0.0
+      , blue  := 0.0
+      , alpha := 0.0
       }.
 
 alpha(#{alpha := SA}, #{alpha := DA}) ->
