b17a624522fff66b919c5e2bc65fb751cf9a2ff9
Nathan Lander <lander89@gmail.com>
2017-06-20 17:25:49 -0500

    Ping pong exercise

---
+++ conc.erl
@@ -0,0 +1,34 @@
+-module(conc).
+
+-export([start/0, say_something/2, ping/2, pong/0]).
+
+ping(0, Pong_PID) ->
+  Pong_PID ! finished,
+  io:format("ping finished~n", []);
+ping(N, Pong_PID) ->
+  Pong_PID ! {ping, self()},
+  receive
+    pong ->
+      io:format("Ping received pong~n", [])
+  end,
+  ping(N - 1, Pong_PID).
+
+pong() ->
+  receive
+    finished ->
+      io:format("pong finished~n", []);
+    {ping, Ping_PID} ->
+      io:format("Pong received ping~n", []),
+      Ping_PID ! pong,
+      pong()
+  end.
+
+say_something(_, 0) ->
+  done;
+say_something(What, Times) ->
+  io:format("~p~n", [What]),
+  say_something(What, Times - 1).
+
+start() ->
+  Pong_PID = spawn(conc, pong, []),
+  spawn(conc, ping, [3, Pong_PID]).
