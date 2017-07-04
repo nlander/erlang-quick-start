e07727908058f4e2cb649b5bee387997f0cdfea5
Nathan Lander <lander89@gmail.com>
2017-07-04 14:00:03 -0500

    Ping pong with linked processes

--- distributed.erl
+++ distributed.erl
@@ -1,17 +1,21 @@
 -module(distributed).
 
--export([start_ping/1, start_pong/0, ping/2, pong/0]).
+-export([start/1, ping/2, pong/0]).
 
-ping(0, Pong_Node) ->
-  io:format("ping finished~n", []);
+ping(N, Pong_PID) ->
+  link(Pong_PID),
+  ping1(N, Pong_PID).
 
-ping(N, Pong_Node) ->
-  {pong, Pong_Node} ! {ping, self()},
+ping1(0, _) ->
+  exit(ping);
+
+ping1(N, Pong_PID) ->
+  Pong_PID ! {ping, self()},
   receive
     pong ->
       io:format("Ping received pong~n", [])
   end,
-  ping(N - 1, Pong_Node).
+  ping1(N - 1, Pong_PID).
 
 pong() ->
   receive
@@ -19,12 +23,8 @@ pong() ->
       io:format("Pong received ping~n", []),
       Ping_PID ! pong,
       pong()
-  after 5000 ->
-      io:format("Pong timed out~n", [])
   end.
 
-start_pong() ->
-  register(pong, spawn(distributed, pong, [])).
-
-start_ping(Pong_Node) ->
-  spawn(distributed, ping, [3, Pong_Node]).
+start(Ping_Node) ->
+  Pong_PID = spawn(distributed, pong, []),
+  spawn(Ping_Node, distributed, ping, [3, Pong_PID]).
